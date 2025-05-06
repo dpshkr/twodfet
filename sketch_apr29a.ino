afd75a0ed7fe917474c67425fa0ffcd512ebf2ec
Pushkar <121485926+dpshkr@users.noreply.github.com>
2025-05-06 09:34:31 +0530

    Update sketch_apr29a.ino

--- sketch_apr29a.ino
+++ sketch_apr29a.ino
@@ -1,23 +1,40 @@
-int sensorValue;
-unsigned long thisTime, initTime;
-unsigned long integrationTime = 1000;
+//#include <OPAMP.h>
+
+/* TIMING PARAMETERS */
+const unsigned int pulseWidth = 10 ; // Transistor turn on pulse width in ms
+const unsigned int integrationTime = 5000; // Integration time in ms
+
+/* PIN VALUES */
+const int transistorControlPin = 7; // Pin connected to transistor
+const int outPin = A0;
+
+
+unsigned long previousTime = 0;
+unsigned long currentTime;
+int outValue;
 
 void setup() {
   // put your setup code here, to run once:
-  initTime = millis();
+  //analogReadResolution(14);
+  //OPAMP.begin(OPAMP_SPEED_LOWSPEED);
+  pinMode(transistorControlPin, OUTPUT);
   Serial.begin(9600);
-  pinMode(7, OUTPUT);
-
 }
 
 void loop() {
-  sensorValue = analogRead(A0);
-  Serial.println(sensorValue);
-  // put your main code here, to run repeatedly:
-  thisTime = millis() - initTime;
-  if (thisTime % integrationTime < 50) {
-    digitalWrite(7, HIGH);
-    delay(10);
-    digitalWrite(7, LOW);
+
+  outValue = analogRead(outPin);
+  Serial.println(outValue);
+
+  currentTime = millis();
+
+  if (currentTime - previousTime >= integrationTime) {
+    digitalWrite(transistorControlPin, HIGH);
+    previousTime = currentTime;
+    while (millis() - currentTime <= pulseWidth) {
+      outValue = analogRead(outPin);
+      Serial.println(outValue);
+    }
+    digitalWrite(transistorControlPin, LOW);
   }
 }
